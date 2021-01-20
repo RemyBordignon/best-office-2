684c3e2d2fe69dc9c825de402c2e18f25d6b1e94
Bordignon <rbordignon@atlassian.com>
2021-01-21 10:55:43 +1100

    GO-12: hi

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <p>HI</p>
+      <p>whats up</p>
       <header className="App-header">
         <p>
           Which syd office is the best? rly
