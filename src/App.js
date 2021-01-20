5e75889f0e1fe8ecc4ccd634993beb1255c4cd7d
Bordignon <rbordignon@atlassian.com>
2021-01-20 14:16:19 +1100

    OCE-4 buttons

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ function App() {
         <p>
           Which office is the best?
         </p>
-        <div style={{display: 'flex'}}>
+        <div style={{display: 'flex'}} className="Vote-button">
           <p>Three four one? {threeFourOne}</p>
           <button onClick={() => setThreeFourOne(threeFourOne + 1)}>Vote</button>
         </div>
 
-        <div style={{display: 'flex'}}>
+        <div style={{display: 'flex'}} className="Vote-button">
           <p>Three four three? {threeFourThree}</p>
           <button onClick={() => setThreeFourThree(threeFourThree + 1)}>Vote</button>
         </div>
 
-        <div style={{display: 'flex'}}>
+        <div style={{display: 'flex'}} className="Vote-button">
           <p>Three six three? {threeSixThree}</p>
           <button onClick={() => setThreeSixThree(threeSixThree + 1)}>Vote</button>
         </div>
