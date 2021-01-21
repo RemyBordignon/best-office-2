044c44d506dab96fd3591cfe087dca07d370180a
Bordignon <rbordignon@atlassian.com>
2021-01-21 11:38:34 +1100

    GO-14: woo GO14

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <p>HI</p>
+      <p>Hi there solaris</p>
       <p>whats up</p>
       <p>Send events plz</p>
       <header className="App-header">
